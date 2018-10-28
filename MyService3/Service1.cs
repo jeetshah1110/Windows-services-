518bac31c17f590b81cbb4c1471404c174436f7a
jeetshah1110 <36303896+jeetshah1110@users.noreply.github.com>
2018-10-28 16:08:12 +0530

    commit

---
+++ MyService3/Service1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace MyService3
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if(!File.Exists(@"C: \Users\Jeet\source\repos\MyService3\bin\Debug\hello.txt"))
+            {
+                File.Create(@"C: \Users\Jeet\source\repos\MyService3\bin\Debug\hello.txt");
+            }
+
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\Jeet\source\repos\MyService3\bin\Debug\hello.txt",append:true))
+            {
+                sw.WriteLine("service started");
+            }
+        }
+
+        protected override void OnStop()
+        {
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\Jeet\source\repos\MyService3\bin\Debug\hello.txt",append:true))
+            {
+                sw.WriteLine("service stopped");
+                
+            }
+        }
+    }
+}
---
+++ MyService4/Service1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace MyService4
+{
+    public partial class Service1 : ServiceBase
+    {
+        public System.Timers.Timer t1;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if(!File.Exists(@"C: \Users\Jeet\Desktop\hello.txt"))
+            {
+                using (File.Create(@"C:\Users\Jeet\Desktop\hello.txt"))
+                {
+
+                }
+                
+            }
+           using (StreamWriter sw = new StreamWriter(@"C:\Users\Jeet\Desktop\hello.txt", append: true))
+            {
+                sw.WriteLine("service running");
+            }
+            t1 = new System.Timers.Timer();
+            t1.Interval = 30000;                //writes every 30 seconds
+            t1.AutoReset = true;
+            t1.Enabled = true;
+            t1.Elapsed += new System.Timers.ElapsedEventHandler(this.WriteFile);
+        }
+
+        protected override void OnStop()
+        {
+            t1.Stop();
+            t1 = null;
+
+        }
+        public void WriteFile(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\Jeet\Desktop\hello.txt",append:true))
+            {
+                sw.WriteLine("service running");
+            }
+
+        }
+    }
+}
